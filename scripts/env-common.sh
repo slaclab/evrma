61a522a13aa29102fc148d97b3114104e66119db
Stanislav Gruden <stanislav.gruden@cosylab.com>
2015-11-13 15:56:40 +0100

    Preparing the release: R1-0-p3

--- scripts/env-common.sh
+++ scripts/env-common.sh
@@ -3,9 +3,9 @@ export TESTS_DIR=.
 export WRITABLE_TMP_DIR=/home/laci/tmp
 mkdir -p $WRITABLE_TMP_DIR
 
-export EVR_DRIVERS_PATH=/afs/slac/g/lcls/package/linuxKernel_Modules/evrmaDriver/R1-0-p2/buildroot-2014.08-glibc-x86_64
-export EVRMA_PATH=/afs/slac/g/lcls/package/evrma/R1-0-p2/buildroot-2014.08-glibc-x86_64
-export EVR_MANAGER_PATH=/afs/slac/g/lcls/package/evrManager/R1-0-p2/buildroot-2014.08-glibc-x86_64
+export EVR_DRIVERS_PATH=/afs/slac/g/lcls/package/linuxKernel_Modules/evrmaDriver/R1-0-p3/buildroot-2014.08-glibc-x86_64
+export EVRMA_PATH=/afs/slac/g/lcls/package/evrma/R1-0-p3/buildroot-2014.08-glibc-x86_64
+export EVR_MANAGER_PATH=/afs/slac/g/lcls/package/evrManager/R1-0-p3/buildroot-2014.08-glibc-x86_64
 
 export PATH=$PATH:$TESTS_DIR:$EVRMA_PATH/bin:$EVR_MANAGER_PATH/bin
 export LD_LIBRARY_PATH= ### static linking...
